6a2531c47f59e1ccd4e7e5d34757f54ff91a802a
PavloDd <pavlo132465798@gmail.com>
2025-02-07 18:43:42 +0200

    "Added serializer for author model"

---
+++ project/authors/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = [
+            'fullname',
+            'bio_text',
+            'style',
+            'theme',
+            'expression_type',
+            'category',
+        ]
