021e27609db015f25000b7f6de8f5bf9a4fcff51
PavloDd <pavlo132465798@gmail.com>
2025-02-14 17:31:05 +0200

    fixed filters

--- project/artpiece/filters.py
+++ project/artpiece/filters.py
@@ -3,7 +3,7 @@ from .models import ArtPiece
 
 
 class ArtPieceFilter(django_filters.FilterSet):
-    fullname = django_filters.CharFilter(field_name="fullname", lookup_expr="icontains")
-    price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
-    price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")   # contain in title
+    price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")  # less than or equal
+    price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")  # greater than or equal
 
