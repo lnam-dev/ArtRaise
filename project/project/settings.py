a9b3e3f3ebc4713e4b1fad4806debcd48243f816
Maksym Salyha <salyhamaks@gmail.com>
2025-02-21 18:00:33 +0200

    commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -130,11 +130,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'artraise',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
