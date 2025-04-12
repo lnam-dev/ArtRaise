2594977b4d8ef103896ad0cac917650ad4df5b36
Mark <markfedina5@gmail.com>
2025-04-12 13:11:03 +0300

    added mega menu from old repo

--- front/src/ui/components/button/button.tsx
+++ front/src/ui/components/button/button.tsx
@@ -1,45 +1,52 @@
-"use client";
+'use client';
 
-import React from "react";
-import Link from "next/link";
-import Arrow from "~/assets/arrow-right.svg";
+import React from 'react';
+import Link from 'next/link';
+import Arrow from '~/assets/arrow-right.svg';
 
 interface ButtonProps {
-	children: React.ReactNode;
-	variant?: "solid";
-	includeArrow?: boolean;
-	className?: string;
-	href?: string;
+  children: React.ReactNode;
+  variant?: 'solid' | 'light';
+  includeArrow?: boolean;
+  className?: string;
+  href?: string;
 }
 
 export default function Button({
-	children,
-	variant = "solid",
-	includeArrow = true,
-	className,
-	href,
-	...props
+  children,
+  variant = 'solid',
+  includeArrow = true,
+  className,
+  href,
+  ...props
 }: ButtonProps) {
-	let style = "";
+  let style = '';
+  let arrowColor = '';
 
-	switch (variant) {
-		case "solid":
-			style = `font-fixel font-medium text-4 text-white bg-black h-14 transition-all duration-300 hover:bg-gray-950.9`;
-			break;
-	}
+  switch (variant) {
+    case 'solid':
+      style = `font-fixel font-medium text-4 text-white bg-black h-14 transition-all duration-300 hover:bg-gray-950.9`;
+      arrowColor = '#FFFFFF';
+      break;
+    case 'light':
+      style = `font-fixel font-medium text-[#62636D] bg-white h-14 `;
+      arrowColor = '#1F1F1F';
+      break;
+  }
 
-	const button = (
-		<button className={`${style} ${className} relative`} {...props}>
-			<span>{children}</span>
-			{includeArrow && (
-				<Arrow
-					className="fill-white absolute right-4 top-1/2 transform -translate-y-1/2"
-					width={32}
-					height={32}
-				/>
-			)}
-		</button>
-	);
+  const button = (
+    <button className={`${style} ${className} relative w-full`} {...props}>
+      <span>{children}</span>
+      {includeArrow && (
+        <Arrow
+          className=' absolute right-4 top-1/2 transform -translate-y-1/2'
+          width={32}
+          height={32}
+          style={{ fill: arrowColor }}
+        />
+      )}
+    </button>
+  );
 
-	return href ? <Link href={href}>{button}</Link> : button;
+  return href ? <Link href={href}>{button}</Link> : button;
 }
--- front/src/ui/components/layout/footer/footer.tsx
+++ front/src/ui/components/layout/footer/footer.tsx
@@ -4,24 +4,25 @@ import { TenantLogo } from '../../../lib/tenant-logo';
 import { useAppContext } from '../../../app-context/provider';
 import { Footer as FooterType } from '~/use-cases/contracts/Footer';
 import Link from '~/bridge/ui/Link';
-
 import ArrowInsert from '~/assets/arrow-insert.svg';
 
-import classes from './footer.module.scss';
+import LnaaLogo from '~/assets/lnaaLogo.svg';
+import FriendsLnaaLogo from '~/assets/friendsLnaaLogo.svg';
+import DepartmentGDLogo from '~/assets/departmentGDLogo.svg';
 
 export const Footer: React.FC<{ footer: FooterType }> = ({ footer }) => {
-  const { state: appContextState } = useAppContext();
-
   return (
-    <footer className={classes.footer}>
-      <div className={classes.containerf}>
-        <div className={classes.logoAndContactSection}>
+    <footer className='w-full mt-[5rem] bg-[#131315] text-white  '>
+      <div className='container mx-auto'>
+        <div className='flex justify-between'>
           {/* Logo Section */}
-          <div className={classes.logoSection}>
-            <h2 className={classes.logoFooter}>ARTRAISE©</h2>
+          <div className='flex-1 items-center'>
+            <h2 className='2xl:text-[8vw] xl:text-[10vw] lg:text-[8vw] lg:text-left md:text-[10vw] text-[16vw] font-fixel font-bold leading-none text-white text-center'>
+              ARTRAISE©
+            </h2>
           </div>
           {/* Contact */}
-          <div className={classes.contact}>
+          <div className='lg:flex flex-col xl:text-[1.25rem] ml-[1rem] space-y-1 font-namu text-grayCustom hidden'>
             <Link to='mailto:friendsofinaa@gmail.com'>
               friendsofinaa@gmail.com
             </Link>
@@ -32,65 +33,83 @@ export const Footer: React.FC<{ footer: FooterType }> = ({ footer }) => {
           </div>
         </div>
 
-        <div className={classes.partners}>
-          <div className={classes.lnaaSection}>
-            <Image src='/lnaaLogo.png' alt='' width={36} height={55} />
-            <p>Львівська національна академія мистецтв</p>
-          </div>
-
-          <div className={classes.friendsLnaaSection}>
-            <Image src='/friendsLnaaLogo.png' alt='' width={22} height={53} />
-            <p>
-              Благодійний фонд <br />
-              Друзі <br />
-              Львівської національної <br />
-              академії мистецтв
+        <div className='flex lg:gap-12 gap-10 pt-6 '>
+          {/* Львівська нац. академія мистецтв */}
+          <div className='flex items-center gap-3 '>
+            <LnaaLogo className='w-auto h-auto' />
+            <p className='lg:flex self-center leading-tight font-museo hidden'>
+              Львівська <br />
+              національна академія <br />
+              мистецтв
             </p>
           </div>
 
-          <div className={classes.departmentGDSection}>
-            <Image src='/departmentGDLogo.png' alt='' width={66} height={79} />
-            <p>Кафедра Графічного дизайну</p>
+          {/* Благодійний фонд */}
+          <div className='flex items-center gap-3'>
+            <FriendsLnaaLogo className='w-auto h-auto' />
+            <p className='lg:block self-center font-museo tracking-tight hidden'>
+              Благодійний фонд <br />
+              <span className='font-extrabold'>
+                Друзі <br />
+                Львівської національної <br />
+                академії мистецтв
+              </span>
+            </p>
+          </div>
+
+          {/* Кафедра графічного дизайну */}
+          <div className='flex items-center gap-3'>
+            <DepartmentGDLogo className='w-auto h-auto' />
+            <p className='lg:flex self-center font-museo leading-tight hidden'>
+              Кафедра <br />
+              Графічного <br />
+              дизайну
+            </p>
           </div>
         </div>
 
         {/* Navigation */}
-        <div className={classes.navigationFooter}>
-          <Link className={classes.mainLink} to='/'>
+        <div className='flex flex-col items-start lg:flex-row lg:flex-1 lg:justify-between lg:items-center lg:mt-10 sm:mt-[0.625rem] font-namu'>
+          <Link
+            className='xl:text-[2rem] lg:text-[1.75rem] lg:text-white text-grayCustom'
+            to='/'
+          >
             Хто ми є?
           </Link>
-          <Link className={classes.minorLink} to='/'>
+          <Link className='text-grayCustom pt-3' to='/'>
             Про нас
           </Link>
-          <Link className={classes.minorLink} to='/'>
+          <Link className='text-grayCustom pt-3' to='/'>
             Зв’язатися з нами
           </Link>
-          <Link className={classes.minorLink} to='/'>
+          <Link className='text-grayCustom pt-3' to='/'>
             Довідник колекціонера
           </Link>
-          <Link className={classes.minorLink} to='/'>
+          <Link className='text-grayCustom pt-3' to='/'>
             Збір на мистецтво
           </Link>
         </div>
 
-        <div className={classes.contactMobile}>
+        <div className='lg:hidden py-[1.25rem] text-[#fff] opacity-50'>
           <Link to='mailto:friendsofinaa@gmail.com'>
             friendsofinaa@gmail.com
           </Link>
           <p>+380 98 859 39 00</p>
         </div>
 
-        <div className={classes.bottomSection}>
-          <div className={classes.bottomLeft}>
-            <p className={classes.bottomLeftFirstP}>
+        <div className='flex justify-between items-center mt-3 py-3 border-t-1 lg:border-t-2 lg:mt-8 lg:pt-3 lg:pb-3 border-[rgba(209,209,209,0.5)]'>
+          <div className='flex lg:gap-[6rem]'>
+            <p className='lg:h-[1rem] lg:text-[0.875rem] lg:tracking-[-5%] lg:opacity-50 font-namu text-grayCustom'>
               © {new Date().getFullYear()}, ArtRaise Services.
             </p>
-            <p className={classes.bottomLeftSecondP}>All Rights Reserved</p>
+            <p className='lg:flex lg:h-[1rem] lg:text-[0.875rem] font-namu text-grayCustom opacity-50 hidden'>
+              All Rights Reserved
+            </p>
           </div>
 
-          <div className={classes.bottomRight}>
+          <div className='flex items-center lg:gap-[9rem]'>
             <Link
-              className={classes.scrollUp}
+              className='flex items-center gap-1 lg:flex lg:h-[1.25rem] lg:text-[1rem] text-grayCustom hidden'
               to=''
               onClick={(e: any) => {
                 e.preventDefault();
@@ -100,7 +119,9 @@ export const Footer: React.FC<{ footer: FooterType }> = ({ footer }) => {
               Догори
               <ArrowInsert />
             </Link>
-            <p>Design by &lt;5</p>
+            <p className='lg:flex lg:mr-[6rem] lg:h-[1rem] lg:text-[0.875rem] text-grayCustom opacity-50 font-namu hidden'>
+              Design by &lt;5
+            </p>
           </div>
         </div>
       </div>
