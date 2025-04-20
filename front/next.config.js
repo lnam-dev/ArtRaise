bab5831217393471975f1774e0c33bf9dffe4961
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-04-20 15:24:38 +0300

    fix: fix bug with svg
    Fixed bug related @svgr library. Added types for *svg

--- front/next.config.js
+++ front/next.config.js
@@ -1,50 +1,48 @@
 const createNextIntlPlugin = require("next-intl/plugin");
-const withImages = require("next-images");
-
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = withNextIntl(
-	withImages({
-		webpack: (config) => {
-			config.resolve.symlinks = false;
+/** @type {import('next').NextConfig} */
+const nextConfig = withNextIntl({
+	webpack(config) {
+		config.resolve.symlinks = false;
 
-			config.module.rules = config.module.rules.filter((rule) => {
-				if (rule.test && rule.test.toString().includes("svg")) {
-					return false;
-				}
-				return true;
-			});
+		// Видаляємо старі svg rule (якщо є)
+		config.module.rules = config.module.rules.filter((rule) => {
+			if (rule.test && rule.test.toString().includes("svg")) {
+				return false;
+			}
+			return true;
+		});
 
-			config.module.rules.push({
-				test: /\.svg$/,
-				use: ["@svgr/webpack"],
-			});
+		// Додаємо SVGR
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
 
-			return config;
-		},
-		reactStrictMode: true,
-		serverExternalPackages: ["your-package"],
-		images: {
-			remotePatterns: [
-				{
-					protocol: "http",
-					hostname: "localhost",
-				},
-			],
-		},
-		devIndicators: {
-			allowedDevOrigins: ["http://192.168.31.89"],
-		},
-		async redirects() {
-			return [
-				{
-					source: "/",
-					destination: "/ua",
-					permanent: false,
-				},
-			];
-		},
-	})
-);
+		return config;
+	},
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+		],
+	},
+	devIndicators: {
+		allowedDevOrigins: ["http://192.168.31.89"],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/ua",
+				permanent: false,
+			},
+		];
+	},
+});
 
 module.exports = nextConfig;
--- front/src/types/svgr.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.svg" {
-	import { SVGProps } from "react";
-	const content: React.FC<SVGProps<SVGSVGElement>>;
-	export default content;
-}
