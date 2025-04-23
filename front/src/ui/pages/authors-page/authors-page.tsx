ea3c3bb3a73e6cbf2d526f8fe27e6ac6eb6c509c
denisk0m <denisko04032006@gmail.com>
2025-04-23 14:49:06 +0300

    fix:refactor authorspage for using getData

---
+++ front/src/ui/pages/authors-page/authors-page.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from "react";
+import AuthorsFilterMenu from "~/ui/components/authors/authors-filtermenu/AuthorsFilterMenu";
+import {TAuthorsPage} from "~/use-cases/contracts/authors-page";
+import AuthorsMapper from "~/ui/pages/authors-page/authors-mapper";
+
+
+const AuthorsPage: FC<TAuthorsPage> = ({authors}) => {
+    return (
+        <div
+            className={
+                "font-namu mt-16 grid grid-cols-4 gap-x-[1rem] w-full h-fit auto-rows-auto px-[3vw]"
+            }>
+            <div className={"col-span-full"}>
+                <AuthorsFilterMenu/>
+                <AuthorsMapper authors ={authors}/>
+            </div>
+        </div>
+    );
+};
+
+export default AuthorsPage;
