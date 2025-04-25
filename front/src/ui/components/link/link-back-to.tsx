157c12c5672a31cf4f6026e8ab1b179f414a1b2f
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-04-25 12:49:47 +0300

    add: breadcrumbs and links
    Added new components: breadcrumbs and back-to-link

---
+++ front/src/ui/components/link/link-back-to.tsx
@@ -0,0 +1,23 @@
+import Link from "~/bridge/ui/Link";
+import ArrowBackTo from "~/assets/arrow-back.svg";
+
+type LinkBackToProps = {
+	path: string;
+	children: string;
+	className?: string;
+};
+
+const LinkBackTo = ({ path, children, className }: LinkBackToProps) => {
+	return (
+		<Link
+			to={path}
+			className={`flex items-center gap-2 pl-4 xl:pl-0 ${className}`}>
+			<ArrowBackTo className="fill-gray-950" height={32} width={32} />
+			<span className="font-fixel font-normal text-4 text-gray-950">
+				{children}
+			</span>
+		</Link>
+	);
+};
+
+export default LinkBackTo;
