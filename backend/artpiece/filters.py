2c90d01f42563b68e363e7966d9419e172b895f3
PavloDd <pavlo132465798@gmail.com>
2025-04-29 12:06:17 +0300

    add price range filter

--- backend/artpiece/filters.py
+++ backend/artpiece/filters.py
@@ -7,6 +7,7 @@ class ArtPieceFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")  # Пошук по частині назви
     price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")  # Максимальна ціна
     price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")  # Мінімальна ціна
+    price_range = django_filters.RangeFilter(field_name="price")
     type = django_filters.ChoiceFilter(field_name="type", choices=ArtPieceType.choices)  # Тип твору мистецтва
     material = django_filters.CharFilter(field_name="material", lookup_expr="icontains")  # Пошук по матеріалу
     theme = django_filters.CharFilter(field_name="theme", lookup_expr="icontains")  # Пошук по темі
