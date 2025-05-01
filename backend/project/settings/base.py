a4df6d427fbf401239fd05a0591249b3024acd09
Maks <80846087+makson2006@users.noreply.github.com>
2025-05-02 00:21:22 +0400

    Update base.py

--- backend/project/settings/base.py
+++ backend/project/settings/base.py
@@ -143,6 +143,9 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
