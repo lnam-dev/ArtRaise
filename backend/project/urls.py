1a173df5a0360461edabf168fd2ac87d43c66d1e
PavloDd <pavlo132465798@gmail.com>
2025-05-03 15:14:53 +0300

    change urls

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -9,17 +9,17 @@ from wagtail.documents import urls as wagtaildocs_urls
 # Основні маршрути вашого додатку та Wagtail
 # Змінені маршути з приставкою API для правильної маршутизації
 urlpatterns = [
-    path("health/", lambda request: HttpResponse("OK", status=200)),
+    path('health/', lambda request: HttpResponse("OK", status=200)),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     # Можливо, 'pages/' краще залишити для catch-all нижче, якщо він обробляє сторінки
     # path('pages/', include(wagtail_urls)),
-    path('users/', include('users.urls')),
-    path('search/', include('search.urls')),
-    path('authors/', include('authors.urls')),
-    path('events/', include('events.urls')),
-    path('artpieces/', include('artpiece.urls')),
-    path('faq/', include('faq.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/search/', include('search.urls')),
+    path('api/authors/', include('authors.urls')),
+    path('api/events/', include('events.urls')),
+    path('api/artpieces/', include('artpiece.urls')),
+    path('api/faq/', include('faq.urls')),
 
     # Wagtail catch-all для сторінок - зазвичай має бути останнім серед основних маршрутів
     re_path(r'^', include(wagtail_urls)),
