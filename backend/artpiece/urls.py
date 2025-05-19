f821ce493b041890bf99953f825b51ac1f91dd40
makson2006 <salyhamaks@gmail.com>
2025-05-19 19:08:24 +0400

    added counter to filters

--- backend/artpiece/urls.py
+++ backend/artpiece/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ArtPieceViewSet
+from .views import ArtPieceViewSet, ArtPieceStatsView
 
 urlpatterns = [
     path('', ArtPieceViewSet.as_view({'get': 'list'})),
-    path('<int:pk>/', ArtPieceViewSet.as_view({'get': 'retrieve'}))
+    path('<int:pk>/', ArtPieceViewSet.as_view({'get': 'retrieve'})),
+    path('stats/', ArtPieceStatsView.as_view(), name='artpiece-stats'),
+
 ]
--- backend/artpiece/views.py
+++ backend/artpiece/views.py
@@ -2,9 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from .models import ArtPiece
 from .serializers import ArtPieceDetailSerializer, ArtPieceSerializer
 from .filters import ArtPieceFilter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import ArtPiece
+from django.db.models import Count
+
 
 class ArtPieceViewSet(ModelViewSet):
     renderer_classes = [JSONRenderer]
@@ -18,3 +22,25 @@ class ArtPieceViewSet(ModelViewSet):
         if self.action == 'list':
             return ArtPieceSerializer
         return ArtPieceDetailSerializer
+
+
+
+class ArtPieceStatsView(APIView):
+    def get(self, request):
+        def to_array(queryset, field_name):
+            return [{"name": item[field_name], "count": item["count"]} for item in queryset]
+
+        type_counts = ArtPiece.objects.values('type').annotate(count=Count('id')).order_by()
+        style_counts = ArtPiece.objects.values('style').annotate(count=Count('id')).order_by()
+        theme_counts = ArtPiece.objects.values('theme').annotate(count=Count('id')).order_by()
+        material_counts = ArtPiece.objects.values('material').annotate(count=Count('id')).order_by()
+        dominant_color_counts = ArtPiece.objects.values('dominant_color').annotate(count=Count('id')).order_by()
+
+        data = {
+            "type": to_array(type_counts, 'type'),
+            "style": to_array(style_counts, 'style'),
+            "theme": to_array(theme_counts, 'theme'),
+            "material": to_array(material_counts, 'material'),
+            "dominant_color": to_array(dominant_color_counts, 'dominant_color'),
+        }
+        return Response(data)
