5807837f7a96ee4e7ceead913bdb12afeac31b89
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-06-09 13:56:09 +0300

    fix: formating qa page

--- front/src/ui/pages/qa-page/qa-page.tsx
+++ front/src/ui/pages/qa-page/qa-page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {TQAPage} from "~/use-cases/contracts/qa-page";
+import React from "react";
+import { TQAPage } from "~/use-cases/contracts/qa-page";
 import BreadcrumbsWrapper from "~/ui/components/breadcrumbs/breadcrumbs-wrapper";
 import BreadcrumbsLink from "~/ui/components/breadcrumbs/breadcrumbs-link";
 import LinkBackTo from "~/ui/components/link/link-back-to";
@@ -11,25 +11,28 @@ import CallToActionSection from "~/ui/components/cta-section/cta-section";
 
 type Props = TQAPage;
 
-const QAPage: React.FC<Props> = ({frequentlyAskedQuestions, questionsCategories}) => {
-    return (
-        <div
-            className={
-                "font-namu mt-16 grid grid-cols-4 gap-x-[3rem] w-full h-fit auto-rows-auto px-[3vw]"
-            }>
-            <BreadcrumbsWrapper activeIndex={2} className="col-span-full mt-9 pl-0">
-                <BreadcrumbsLink>Головна</BreadcrumbsLink>
-                <BreadcrumbsLink>Зазвичай запитують</BreadcrumbsLink>
-            </BreadcrumbsWrapper>
-            <LinkBackTo path="/" className="mt-4 text-nowrap mb-6 pl-10 opacity-70">
-                до головної
-            </LinkBackTo>
-            <MostCommonQuestion/>
-            <FrequentQuestionsMapper frequentQuestions={frequentlyAskedQuestions}/>
-            <QACategoriesMapper categories={questionsCategories}/>
-            <CallToActionSection className={"col-span-full"}/>
-        </div>
-    );
+const QAPage: React.FC<Props> = ({
+	frequentlyAskedQuestions,
+	questionsCategories,
+}) => {
+	return (
+		<div
+			className={
+				"font-namu mt-16 grid grid-cols-4 gap-x-[3rem] w-full h-fit auto-rows-auto px-[3vw]"
+			}>
+			<BreadcrumbsWrapper activeIndex={2} className="col-span-full mt-9 pl-0">
+				<BreadcrumbsLink>Головна</BreadcrumbsLink>
+				<BreadcrumbsLink>Зазвичай запитують</BreadcrumbsLink>
+			</BreadcrumbsWrapper>
+			<LinkBackTo path="/" className="mt-4 text-nowrap mb-6 pl-10 opacity-70">
+				до головної
+			</LinkBackTo>
+			<MostCommonQuestion />
+			<FrequentQuestionsMapper frequentQuestions={frequentlyAskedQuestions} />
+			<QACategoriesMapper categories={questionsCategories} />
+			<CallToActionSection className={"col-span-full"} />
+		</div>
+	);
 };
 
 export default QAPage;
