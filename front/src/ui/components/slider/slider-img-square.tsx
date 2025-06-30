0408eda8f8edc0af1c8c369f147281824a8aadba
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-06-30 15:24:13 +0300

    fix: slider square
    Finally fixed slider for square artpices

--- front/src/ui/components/slider/slider-img-square.tsx
+++ front/src/ui/components/slider/slider-img-square.tsx
@@ -1,37 +1,37 @@
 import Image from "next/image";
 import { TSlide } from "~/types/slider";
+import useDevice from "~/ui/hooks/useDevice";
 
 type SliderImgPortraitSquare = Pick<TSlide, "imgSrc"> & {
 	index: number;
 };
 
 const SliderImgSquare = ({ imgSrc, index }: SliderImgPortraitSquare) => {
+	const { isTablet, isDesktop } = useDevice();
+
+	const columnsCount = isTablet ? 1 : isDesktop ? 2 : 0;
+
 	return (
 		<div className="flex flex-row justify-end gap-2 md:gap-4 xl:gap-6 max-h-[90vh] lg:max-h-[75vh] xl:max-h-[70vh]">
-			<figure className="flex-grow-[1] relative aspect-square">
+			{Array.from({ length: columnsCount }, () => (
+				<figure className="flex-grow-[2] relative aspect-square">
+					<Image
+						src={imgSrc}
+						alt={`Slide`}
+						className="object-cover opacity-30"
+						fill
+						loading="lazy"
+					/>
+				</figure>
+			))}
+			<figure className="flex-grow-[2] flex-shrink-0 w-auto h-auto relative">
 				<Image
 					src={imgSrc}
 					alt={`Slide ${index + 1}`}
-					className="h-auto w-full object-cover opacity-30"
-					fill
-					loading="lazy"
-				/>
-			</figure>
-			<figure className="flex-grow-[1] relative aspect-square">
-				<Image
-					src={imgSrc}
-					alt={`Slide ${index + 1}`}
-					className="h-auto w-full object-cover opacity-30"
-					fill
-					loading="lazy"
-				/>
-			</figure>
-			<figure className="flex-grow-[2] flex-shrink-0 relative aspect-square">
-				<Image
-					src={imgSrc}
-					alt={`Slide ${index + 1}`}
-					className="h-full w-auto object-cover object-right"
-					fill
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="w-full h-auto object-contain object-right"
 					loading="lazy"
 				/>
 			</figure>
