21624ada2d126b10c657c7064a145d36843143c5
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-06-30 16:44:25 +0300

    fix: working variant

--- front/src/ui/components/slider/slider-img-portrait.tsx
+++ front/src/ui/components/slider/slider-img-portrait.tsx
@@ -10,25 +10,20 @@ const COLUMNS_IMAGES = [
 	["30%", "70%"],
 	["70%", "30%"],
 	["30%", "70%"],
+	["70%", "30%"],
+	["30%", "70%"],
 ];
 
 const SliderImgPortrait = ({ imgSrc, index }: SliderImgPortraitProps) => {
 	const { isDesktop } = useDevice();
 	return (
-		<div className="flex flex-row justify-start gap-6 h-[70vh]">
-			<figure className="relative w-full lg:max-w-[50%] xl:max-w-[25%] aspect-[9/16]">
-				<Image
-					src={imgSrc}
-					alt={`Slide ${index + 1}`}
-					className="object-cover"
-					fill
-					loading="lazy"
-				/>
-			</figure>
-			{isDesktop && (
-				<div className="columns-3 gap-6 w-full">
+		<div className="flex flex-row justify-end gap-2 md:gap-4 xl:gap-6 max-h-[75vh]">
+			{
+				<div className="columns-1 sm:columns-2 xl:columns-3 2xl:columns-4 gap-2 md:gap-4 xl:gap-6 w-full display-none sm:display-block">
 					{COLUMNS_IMAGES.map((heights, colIdx) => (
-						<div key={colIdx} className="space-y-6 w-full h-full">
+						<div
+							key={colIdx}
+							className="space-y-2 md:space-y-4 xl:space-y-6 w-full h-full">
 							{heights.map((height, imgIdx) => {
 								return (
 									<figure
@@ -50,7 +45,18 @@ const SliderImgPortrait = ({ imgSrc, index }: SliderImgPortraitProps) => {
 						</div>
 					))}
 				</div>
-			)}
+			}
+			<figure className="flex-grow-[3] flex-shrink-0 w-auto h-auto relative min-w-[40vh] md:min-w-[45vh] lg:min-w-[50vh] lg:max-w-[50%] xl:max-w-[25%]">
+				<Image
+					src={imgSrc}
+					alt={`Slide ${index + 1}`}
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="w-full h-auto"
+					loading="lazy"
+				/>
+			</figure>
 		</div>
 	);
 };
