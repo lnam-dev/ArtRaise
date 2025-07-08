e0726243c6d2c786e7ca28d319814721bfc81a9e
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-07-08 17:44:08 +0000

    доданий enpoint для форми покупки

--- backend/artpiece/urls.py
+++ backend/artpiece/urls.py
@@ -4,6 +4,7 @@ from .views import ArtPieceViewSet, ArtPieceStatsView
 urlpatterns = [
     path('', ArtPieceViewSet.as_view({'get': 'list'})),
     path('<int:pk>/', ArtPieceViewSet.as_view({'get': 'retrieve'})),
+    path('<int:pk>/send-buy-form/', ArtPieceViewSet.as_view({'post': 'send_buy_form'})),
+    path('<int:pk>/buy-requests/', ArtPieceViewSet.as_view({'get': 'buy_requests'})),
     path('stats/', ArtPieceStatsView.as_view(), name='artpiece-stats'),
-
 ]
