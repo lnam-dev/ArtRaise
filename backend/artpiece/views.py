db8c8bf0f386995fbf1f2a7b9fa1fed95f9b50ec
EgorkaKv <yehor.kvasenko@gmail.com>
2025-07-10 23:07:46 +0300

    remove unused import from artpiece/views.py

--- backend/artpiece/views.py
+++ backend/artpiece/views.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.renderers import JSONRenderer
 from django_filters.rest_framework import DjangoFilterBackend
