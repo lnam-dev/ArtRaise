4957da56b81c0d0049069c0b4264d44b1b9d9b8d
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-07-29 00:12:23 +0300

    feat: implement dynamic category pages and refactor categories
    Added dynamic category page route and component to display art pieces by category. Refactored category types and data fetching to use API responses, updated UI components to use new TCategory structure, and improved category card logic. Updated dependencies and improved product page layout and animation with GSAP integration.

---
+++ front/src/app/[locale]/categories/[category]/page.tsx
@@ -0,0 +1,36 @@
+import { TCategory } from "~/types/categories";
+import { TArtPiece } from "~/types";
+
+import CategoryPage from "~/ui/pages/category-page/category-page";
+
+export type TParams = {
+	params: Promise<{
+		category: string;
+	}>;
+};
+
+export const revalidate = 21600;
+export const dynamic = "force-dynamic";
+
+async function getData(category: string): Promise<TArtPiece[]> {
+	try {
+		const response = await fetch(
+			`${process.env.API_URL}search?type=${category}`
+		);
+		if (!response.ok) {
+			throw new Error(`Failed to fetch artpieces: ${response.status}`);
+		}
+		const { artPieces }: { artPieces: TArtPiece[] } = await response.json();
+		return artPieces;
+	} catch (error) {
+		console.error(`Помилка при завантаженні арт-об'єктів: ${error}`);
+		return [];
+	}
+}
+
+export default async ({ params }: TParams) => {
+	const { category } = await params;
+	const artPieces = await getData(category);
+
+	return <CategoryPage category={category} artPieces={artPieces} />;
+};
