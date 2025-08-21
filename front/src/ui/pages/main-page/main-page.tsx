c8a8cd454a2a3a3393b08ee8a42a92606cb90794
denisk0m <denisko04032006@gmail.com>
2025-08-21 22:03:40 +0300

    fix : minor fixes

--- front/src/ui/pages/main-page/main-page.tsx
+++ front/src/ui/pages/main-page/main-page.tsx
@@ -3,7 +3,7 @@ import SearchBar from "../../components/search-bar/search-bar";
 import SegmentTitle from "../../components/segment-title/segment-title";
 import CardPurchase from "../../components/card/card-purchase";
 import CertificatesSection from "../../components/certificatesSection/certificate-section";
-import PartnersSection from "../../components/partners-section/partners-section";
+import AboutFund from "../../components/partners-section/about-fund";
 import CallToActionSection from "../../components/cta-section/cta-section";
 import MainSliderWrapper from "./main-slider-wrapper";
 
@@ -21,33 +21,12 @@ export default ({ artPieces, slides }: TMainPage) => {
 							Нові надходження
 						</SegmentTitle>
 						<div className="columns-1 gap-6 sm:columns-2 lg:columns-3 xl:columns-4 space-y-12">
-							{artPieces.map((obj) => (
+							{artPieces.slice(0,7).map((obj) => (
 								<CardPurchase key={obj.id} card={obj} />
 							))}
 						</div>
 					</section>
-					<section>
-						<SegmentTitle
-							className="mb-10"
-							link={{ to: "/", name: "всі рекомендовані" }}>
-							Рекомендації
-						</SegmentTitle>
-					</section>
-					<section>
-						<SegmentTitle
-							className="mb-10"
-							link={{ to: "/", name: "всі з фонду" }}>
-							Фонд
-						</SegmentTitle>
-					</section>
-					<section>
-						<SegmentTitle
-							className="mb-10"
-							link={{ to: "/", name: "Про фонд" }}>
-							Фонд
-						</SegmentTitle>
-					</section>
-					<PartnersSection className="mb-10" />
+					<AboutFund className="mb-10" />
 					<CertificatesSection />
 					<CallToActionSection />
 				</article>
