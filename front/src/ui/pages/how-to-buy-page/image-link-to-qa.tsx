59a9a8026e747384bc209f66a8e4b28a7ea85843
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-09-11 23:11:22 +0300

    fix: qa and question and answer page

--- front/src/ui/pages/how-to-buy-page/image-link-to-qa.tsx
+++ front/src/ui/pages/how-to-buy-page/image-link-to-qa.tsx
@@ -5,22 +5,26 @@ import ButtonTransparent from "~/ui/components/button/button-transparent";
 
 const ImageLinkToQA = () => {
 	return (
-		<div className="col-span-full flex relative items-center justify-center md:justify-start overflow-x-hidden  mb-12 md:mb-14">
-			<figure>
+		<div className="col-span-full flex relative items-center justify-center md:justify-start overflow-hidden mb-12 md:mb-14">
+			<figure
+				className={"relative w-full aspect-[5/1] max-h-[80vh]"}
+				data-orientation={"landscape"}
+				data-target-aspect={5 / 1}>
 				<Image
-					src="/QA/QAPageBackGround.png"
-					height={200}
-					width={1500}
+					src="/how-to-buy/kozlov_store.webp"
 					alt="Фонове зображення для сторінки Q&A"
-					className="object-cover"
+					fill
+					className="object-cover object-center"
 					loading="lazy"
+					sizes="100vw"
+					priority={false}
 				/>
 			</figure>
 			<ButtonTransparent
 				href="/questions-and-answers"
 				primaryText="Маєте інші питання?"
 				secondaryText="« Найчастіші питання »"
-				description="	Тицяй сюди якщо шукаєш відповіді!"
+				description="\tТицяй сюди якщо шукаєш відповіді!"
 				className="absolute mx-4 md:ml-[10%]"
 			/>
 		</div>
