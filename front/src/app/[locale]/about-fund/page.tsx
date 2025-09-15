b6c36bf1b2c1572686554f6ad1ba914d0693ddeb
denisk0m <denisko04032006@gmail.com>
2025-09-16 00:16:26 +0300

    fix: about fund page

---
+++ front/src/app/[locale]/about-fund/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutFundPage from "~/ui/pages/about-fund-page/about-fund-page";
+
+const Page = () => {
+    return (
+        <AboutFundPage/>
+    );
+};
+
+export default Page;
